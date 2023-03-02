af8550b90988aa1f56cd3773a9a29f18113176fd
Loc Nguyen <tanlocit20062000@gmail.com>
2023-03-02 15:29:45 +0700

    a

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const dotenv = require("dotenv").config();
 const authRouter = require("./routes/authRoute");
-
 const PORT = process.env.PORT || 5000;
+const cookieParser = require("cookie-parser");
 dbConnect();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/user", authRouter);
 
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,15 +5,24 @@ const {
   getAllUser,
   getUserById,
   deleteUser,
-  updateInformationUser
+  updateInformationUser,
+  blockUser,
+  unblockUser,
+  handleRefreshToken,
+  logout,
 } = require("../controller/userController");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
-router.get("/get-all-user", getAllUser);
-router.get("/:id", getUserById);
+router.get("/get-all-user", authMiddleware, isAdmin, getAllUser);
+router.get("/refresh", handleRefreshToken);
+router.get("/logout", logout);
+router.get("/:id", authMiddleware, isAdmin, getUserById);
 router.delete("/:id", deleteUser);
-router.put("/:id", updateInformationUser);
+router.put("/edit-user", authMiddleware, updateInformationUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
 
 module.exports = router;
